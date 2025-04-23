fc12161e2144df3a9932768f637e2ce312ab08d8
MR-S4NM1 <miguelitogarcia30@outlook.com>
2025-04-22 21:52:40 -0600

    Crane Game Update I_04-22-2025

--- Assets/Code/CraneManager.cs
+++ Assets/Code/CraneManager.cs
@@ -7,12 +7,16 @@ public class CraneManager : MonoBehaviour
 {
     [SerializeField] protected XRJoystick _joystick;
     [SerializeField] protected XRSlider _slider;
+    [SerializeField] protected XRGripButton _gripButtonA;
+    [SerializeField] protected XRGripButton _gripButonB;
     [SerializeField] protected float _craneMovementSpeed;
     [SerializeField] protected Rigidbody _rb;
     protected Animator _animator;
     protected Vector3 _input;
-    Vector3 _direction;
-    bool _buttonPressed;
+    protected Vector3 _direction;
+    protected bool _grabButtonPressed;
+    protected bool _yButtonPressed;
+    protected bool _grabObject;
 
     void Start(){
         _rb = GetComponent<Rigidbody>();
@@ -21,39 +25,73 @@ public class CraneManager : MonoBehaviour
 
     private void OnEnable(){
         _joystick.onValueChangeX.AddListener(UpdateX);
-        _joystick.onValueChangeX.AddListener(UpdateZ);
+        _joystick.onValueChangeY.AddListener(UpdateZ);
         _slider.onValueChange.AddListener(UpdateY);
     }
 
     private void OnDisable(){
         _joystick.onValueChangeX.RemoveListener(UpdateX);
-        _joystick.onValueChangeX.RemoveListener(UpdateZ);
+        _joystick.onValueChangeY.RemoveListener(UpdateZ);
         _slider.onValueChange.RemoveListener(UpdateY);
     }
 
     public void ModifyButtonInfo()
     {
-        if (!_buttonPressed)
+        if (!_grabButtonPressed)
         {
+            _grabObject = true;
             _animator.SetTrigger("Grab");
         }
         else
         {
+            _grabObject = false;
             _animator.SetTrigger("Release");
         }
-        _buttonPressed = !_buttonPressed;
+        _grabButtonPressed = !_grabButtonPressed;
+    }
+
+    public void ModifyYAxisOfTheCrane()
+    {
+        if (!_yButtonPressed) UpdateY(-1.0f);
+        else UpdateY(1.0f);
+
+        _yButtonPressed = !_yButtonPressed;
     }
 
     private void FixedUpdate(){
-        if (Mathf.Abs(_input.y) == 1){
-            _direction = new Vector3(_input.x, _input.y, _input.z);
-        }
-        else{
-            _direction = new Vector3(_input.x, 0.0f, _input.z);
-        }
+        _direction = new Vector3(_input.x, _input.y, _input.z);
 
         _rb.MovePosition(_rb.position + _direction.normalized * 
             _craneMovementSpeed * Time.fixedDeltaTime);
+
+        if(_rb.position.y <= 2.5f)
+        {
+            _rb.position = new Vector3(_rb.position.x, 2.55f, _rb.position.z);
+            _yButtonPressed = true;
+            UpdateY(0.0f);
+        }
+        else if(_rb.position.y >= 5.5f)
+        {
+            _rb.position = new Vector3(_rb.position.x, 5.45f, _rb.position.z);
+            _yButtonPressed = false;
+            UpdateY(0.0f);
+        }
+        else if(_rb.position.z <= 3.0f)
+        {
+            _rb.position = new Vector3(_rb.position.x, _rb.position.y, 3.05f);
+        }
+        else if (_rb.position.z >= 10.0f)
+        {
+            _rb.position = new Vector3(_rb.position.x, _rb.position.y, 9.95f);
+        }
+        else if (_rb.position.x <= 7.0f)
+        {
+            _rb.position = new Vector3(7.05f, _rb.position.y, _rb.position.z);
+        }
+        else if (_rb.position.x >= 13.0f)
+        {
+            _rb.position = new Vector3(12.95f, _rb.position.y, _rb.position.z);
+        }
     }
 
     protected void UpdateX(float p_xMovement){
@@ -66,5 +104,10 @@ public class CraneManager : MonoBehaviour
 
     protected void UpdateZ(float p_zMovement){
         _input.z = p_zMovement;
+    }
+
+    public bool GetGrabObject 
+    {  
+        get { return _grabObject; }
     }
 }
--- Assets/Code/GrabObj.cs
+++ Assets/Code/GrabObj.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class GrabObj : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] protected CraneManager _craneManager;
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Object")){
-            other.transform.SetParent(transform, true);
-            other.transform.position = transform.position;
-            other.GetComponent<Rigidbody>().isKinematic = true;
+        if (_craneManager.GetGrabObject)
+        {
+            if (other.CompareTag("Object"))
+            {
+                other.transform.SetParent(transform, true);
+                other.transform.position = transform.position;
+                other.GetComponent<Rigidbody>().isKinematic = true;
+                other.GetComponent<Rigidbody>().useGravity = false;
+            }
+        }
+        else
+        {
+            if (other.CompareTag("Object"))
+            {
+                other.transform.SetParent(null);
+                other.GetComponent<Rigidbody>().isKinematic = false;
+                other.GetComponent<Rigidbody>().useGravity = true;
+            }
         }
     }
 }
